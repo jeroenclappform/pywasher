6a4138e1ed9dee2d284c7fecefdf134cf1482389
stanbekker17@gmail.com <stanbekker17@gmail.com>
2021-04-29 12:02:38 +0200

    explore_column_names layout tweak and list bug fixed

--- pywasher/app.py
+++ pywasher/app.py
@@ -162,7 +162,7 @@ class App:
     @property
     def explore_datatypes(self):
         df = self.df
-        print(f"{'index' : <5}{'type' : ^10}{'dtype' : ^10}{'nulls' : ^5}{'different values': ^20}{'column' : <10}")
+        print(f"{'index' : <8}{'type' : <10}{'dtype' : <8}{'nulls' : <6}{'differences': <12}{'column' : <1}")
         for i in df:
             try:
                 a = df[i].unique()
@@ -179,7 +179,12 @@ class App:
                     type = 'datetime'
                 else:
                     type = 'multiple'
-            print(f"{df.columns.get_loc(i) : <5}{type : ^10}{df[i].dtype.name : ^10}{df[i].isna().sum(): ^5}{df[i].count(): ^20}{i : <10}")
+            if type == 'list':
+                print(
+                    f"{df.columns.get_loc(i) : <8}{type : <10}{df[i].dtype.name: <8}{df[i].isna().sum(): <6}{'NaN': <12}{i : <1}")
+            else:
+                print(
+                    f"{df.columns.get_loc(i) : <8}{type : <10}{df[i].dtype.name: <8}{df[i].isna().sum(): <6}{df[i].nunique(): <12}{i : <1}")
         pass
 
     def column_merge(self, columns, delete = False):
